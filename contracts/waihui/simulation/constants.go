23801c7e06fc3adab3d80b5522b350e779ed701d
Bryce Weiner <bryce.weiner@alt.market>
2020-04-01 10:10:06 -0700

    Waihui team addresses

--- contracts/waihui/simulation/constants.go
+++ contracts/waihui/simulation/constants.go
@@ -36,27 +36,24 @@ var (
 
 	TokenNameList = []string{"BTC", "TAO", "XRP", "LTC", "BNB", "ADA", "ETC", "BCH", "EOS", "USD"}
 	TeamAddresses = []common.Address{
-		common.HexToAddress("0x8fB1047e874d2e472cd08980FF8383053dd83102"), // MM team
-		common.HexToAddress("0x9ca1514E3Dc4059C29a1608AE3a3E3fd35900888"), // MM team
-		common.HexToAddress("0x15e08dE16f534c890828F2a0D935433aF5B3CE0C"), // CTO
-		common.HexToAddress("0xb68D825655F2fE14C32558cDf950b45beF18D218"), // DEX team
-		common.HexToAddress("0xF7349C253FF7747Df661296E0859c44e974fb52E"), // HaiDV
-		common.HexToAddress("0x9f6b8fDD3733B099A91B6D70CDC7963ebBbd2684"), // Can
-		common.HexToAddress("0x06605B28aab9835be75ca242a8aE58f2e15F2F45"), // Nien
-		common.HexToAddress("0x33c2E732ae7dce8B05F37B2ba0CFe14c980c4Dbe"), // Vu Pham
-		common.HexToAddress("0x16a73f3a64eca79e117258e66dfd7071cc8312a9"), // BTCTAO
-		common.HexToAddress("0xac177441ac2237b2f79ecff1b8f6bca39e27ef9f"), // ETHTAO
-		common.HexToAddress("0x4215250e55984c75bbce8ae639b86a6cad8ec126"), // XRPTAO
-		common.HexToAddress("0x6b70ca959814866dd5c426d63d47dde9cc6c32d2"), // LTCTAO
-		common.HexToAddress("0x33df079fe9b9cd7fb23a1085e4eaaa8eb6952cb3"), // BNBTAO
-		common.HexToAddress("0x3cab8292137804688714670640d19f9d7a60c472"), // ADATAO
-		common.HexToAddress("0x9415d953d47c5f155cac9de7b24a756f352eafbf"), // ETCTAO
-		common.HexToAddress("0xe32d2e7c8e8809e45c8e2332830b48d9e231e3f2"), // BCHTAO
-		common.HexToAddress("0xf76ddbda664ea47088937e1cf9ff15036714dee3"), // EOSTAO
-		common.HexToAddress("0xc465ee82440dada9509feb235c7cd7d896acf13c"), // ETHBTC
-		common.HexToAddress("0xb95bdc136c579dc3fd2b2424a8e925a90228d2c2"), // XRPBTC
-		common.HexToAddress("0xe36c1842365595D44854eEcd64B11c8115E133EF"), // USDTAO
-		common.HexToAddress("0xaaC1959F6F0fb539F653409079Ec4146267B7555"), // BTCUSD
-		common.HexToAddress("0x726DA688e2e09f01A2e1aB4c10F25B7CEdD4a0f3"), // ETHUSD
+		common.HexToAddress("0x197d65cbdc63d7257ada750ade05201951d74f63"), // team
+		common.HexToAddress("0xdee58223da4d35314aecf44e296ebd88f796b385"), // team
+		common.HexToAddress("0xf9680de94f0a39f948088c41fa9228f60f4145b2"), // team
+		common.HexToAddress("0x741d61bdfbd109dbd32f1163713f718de85c441e"), // team
+		common.HexToAddress("0xb9dc62e5647986f444d60269bef3c3a72346d424"), // team
+		//common.HexToAddress("0x16a73f3a64eca79e117258e66dfd7071cc8312a9"), // BTCTAO
+		//common.HexToAddress("0xac177441ac2237b2f79ecff1b8f6bca39e27ef9f"), // ETHTAO
+		//common.HexToAddress("0x4215250e55984c75bbce8ae639b86a6cad8ec126"), // XRPTAO
+		//common.HexToAddress("0x6b70ca959814866dd5c426d63d47dde9cc6c32d2"), // LTCTAO
+		//common.HexToAddress("0x33df079fe9b9cd7fb23a1085e4eaaa8eb6952cb3"), // BNBTAO
+		//common.HexToAddress("0x3cab8292137804688714670640d19f9d7a60c472"), // ADATAO
+		//common.HexToAddress("0x9415d953d47c5f155cac9de7b24a756f352eafbf"), // ETCTAO
+		//common.HexToAddress("0xe32d2e7c8e8809e45c8e2332830b48d9e231e3f2"), // BCHTAO
+		//common.HexToAddress("0xf76ddbda664ea47088937e1cf9ff15036714dee3"), // EOSTAO
+		//common.HexToAddress("0xc465ee82440dada9509feb235c7cd7d896acf13c"), // ETHBTC
+		//common.HexToAddress("0xb95bdc136c579dc3fd2b2424a8e925a90228d2c2"), // XRPBTC
+		//common.HexToAddress("0xe36c1842365595D44854eEcd64B11c8115E133EF"), // USDTAO
+		//common.HexToAddress("0xaaC1959F6F0fb539F653409079Ec4146267B7555"), // BTCUSD
+		//common.HexToAddress("0x726DA688e2e09f01A2e1aB4c10F25B7CEdD4a0f3"), // ETHUSD
 	}
 )
